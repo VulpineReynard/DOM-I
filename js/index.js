93807dc8a6a3a820a08f69e6e34ac2e56eab2871
VulpineReynard <milare8@gmail.com>
2019-06-17 15:22:08 -0400

    Added base structure using Javacsript

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const siteContent = {
     "img-src": "img/logo.png"
   },
   "cta": {
-    "h1": "DOM Is Awesome",
+    "h1": "DOM \n Is \n Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -36,7 +36,58 @@ const siteContent = {
     "copyright" : "Copyright Great Idea! 2018"
   },
 };
+const linebreak = document.createElement("br");
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const navigation = document.querySelectorAll('a');
+console.log(navigation);
+
+navigation[0].textContent = siteContent.nav["nav-item-1"];
+navigation[1].textContent = siteContent.nav["nav-item-2"];
+navigation[2].textContent = siteContent.nav["nav-item-3"];
+navigation[3].textContent = siteContent.nav["nav-item-4"];
+navigation[4].textContent = siteContent.nav["nav-item-5"];
+navigation[5].textContent = siteContent.nav["nav-item-6"];
+
+// Update the Masthead Text
+document.querySelector('.cta-text h1').textContent = siteContent.cta.h1;
+
+// Update Button Text
+document.querySelector(".cta-text button").textContent = siteContent.cta["button"];
+
+// Update Site Img
+document.querySelector('.cta img').setAttribute('src', siteContent.cta["img-src"]);
+
+// Top Content
+let topContentHeaders = document.querySelectorAll('.top-content div h4');
+topContentHeaders[0].textContent = siteContent["main-content"]["features-h4"];
+topContentHeaders[1].textContent = siteContent["main-content"]["about-h4"];
+
+let topContentText = document.querySelectorAll('.top-content div p');
+topContentText[0].textContent = siteContent["main-content"]["features-content"];
+topContentText[1].textContent = siteContent["main-content"]["about-content"];
+
+// Update middle img
+document.querySelector('.middle-img').setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+// Bottom Content
+let bottomContentHeaders = document.querySelectorAll('.bottom-content div h4');
+bottomContentHeaders[0].textContent = siteContent["main-content"]['services-h4'];
+bottomContentHeaders[1].textContent = siteContent["main-content"]['product-h4'];
+bottomContentHeaders[2].textContent = siteContent["main-content"]['vision-h4'];
+
+let bottomContentText = document.querySelectorAll('.bottom-content div p');
+bottomContentText[0].textContent = siteContent["main-content"]['services-content'];
+bottomContentText[1].textContent = siteContent["main-content"]['product-content'];
+bottomContentText[2].textContent = siteContent["main-content"]['vision-content'];
+
+//Contact Info
+document.querySelector('.contact h4').textContent = siteContent.contact["contact-h4"];
+document.querySelectorAll('.contact p')[0].textContent = siteContent.contact.address;
+document.querySelectorAll('.contact p')[1].textContent = siteContent.contact.phone;
+document.querySelectorAll('.contact p')[2].textContent = siteContent.contact.email;
+
+document.querySelector('footer p').textContent = siteContent.footer.copyright;
